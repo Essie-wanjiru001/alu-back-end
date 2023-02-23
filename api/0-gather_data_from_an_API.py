4dfacc63dae904c7b57258f8655ea74631a91a45
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2023-02-23 20:19:25 +0000

    api project

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    python script that returns TODO list progress for a given employee ID
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+        request user info by employee ID
+    
